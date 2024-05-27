b161de001553740fac1aaea823be9637527076b4
Aryan Sharma <aryansharma2k4@gmail.com>
2024-05-27 17:50:59 +0530

    Higher Order Array Loops

--- highOrderArrayLoops.js
+++ highOrderArrayLoops.js
@@ -77,3 +77,43 @@ languages.forEach((item) => {
 languages.forEach((item) => {
     console.log(item.fileName)
 })
+
+const city = ["dhm", "shm", "mnd"]
+
+//forEach function in js can be stored in another variable but i does not return any value to the variable is stored in
+const values = city.forEach((item) => {
+    console.log(item)
+    return item
+})
+
+//filter operation takes the returned value and can be stored in a new variable
+const myNums = [1,2,3,4,5,6,7,8]
+
+const newNums = myNums.filter ((num) => num >4)
+console.log(newNums)
+
+
+//if using a {} then a new scope is started therefore you need to use a return statement 
+//without the {} there is no need to use a return statement 
+const nNums = myNums.filter ((num) => {
+    return num>4
+})
+
+console.log(nNums);
+
+//same way of doing filter function with forEach loop
+myNums.forEach( (num) => {
+    if( num>4){
+    }
+})
+console.log(newNums)
+
+//reduce function
+const arr1 = [1,2,3,4,5]
+
+//this will add the elements in the array
+//this will initialize the acc with a value and then after every time the we proceed in the array the acc adds with the current value 
+const iniVal = 0
+const myTotal = arr1.reduce( (acc, currVal) => acc + currVal, 0)
+
+console.log(myTotal);
